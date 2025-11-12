7b97c224c6ac7573e0c7933de28527dabee08e70
JusiBridou <juslamarcade@gmail.com>
2025-11-12 14:19:41 +0100

    Amélioration de la strcture, hpp

--- Interface/src/BoatManager.cpp
+++ Interface/src/BoatManager.cpp
@@ -6,10 +6,53 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-/**
- * Fonction pour charger les fichiers json dans des objets Boat
- * @param string le chemin d'accès pour accéder au dossier qui contient les sous-dossiers des bateaux qui contiennent les boat.json
- */
+// Implémentation des parseurs
+
+void ViewList::fromJson(const Json::Value& json) {
+    for (const auto& item : json) {
+        if (item.size() >= 3) {
+            View v;
+            v.x = item[0].asFloat();
+            v.y = item[1].asFloat();
+            v.z = item[2].asFloat();
+            v.elevated = (item.size() > 3) ? item[3].asBool() : false;
+            views.push_back(v);
+        }
+    }
+}
+
+void SailList::fromJson(const Json::Value& json) {
+    const auto& list = json["list"];
+    for (const auto& item : list) {
+        if (item.size() >= 3) {
+            Sail s;
+            s.x = item[0].asFloat();
+            s.y = item[1].asFloat();
+            s.z = item[2].asFloat();
+            sails.push_back(s);
+        }
+    }
+    type = json.get("type", "").asString();
+    size = json.get("size", "").asString();
+}
+
+void PanoList::fromJson(const Json::Value& json) {
+    const auto& files = json["file"];
+    const auto& yaws = json["yaw"];
+    const auto& pitches = json["pitch"];
+    const auto& rolls = json["roll"];
+
+    int count = min({ files.size(), yaws.size(), pitches.size(), rolls.size() });
+    for (int i = 0; i < count; ++i) {
+        file.push_back(files[i].asString());
+        yaw.push_back(yaws[i].asFloat());
+        pitch.push_back(pitches[i].asFloat());
+        roll.push_back(rolls[i].asFloat());
+    }
+}
+
+// Lecture des bateaux
+
 vector<Boat> BoatManager::loadBoats(const string& folderPath) {
     vector<Boat> boats;
 
@@ -37,28 +80,23 @@ vector<Boat> BoatManager::loadBoats(const string& folderPath) {
         b.maxDepth = root.get("MaxDepth", 0.0).asFloat();
         b.makeTransparent = root.get("MakeTransparent", false).asBool();
 
-        // Views
-        b.viewCount = 0;
-        for (const auto& view : root["View"]) {
-            if (b.viewCount >= MAX_VIEWS) break;
-            for (int i = 0; i < 4; ++i) {
-                b.views[b.viewCount][i] = i < view.size() ? view[i].asFloat() : 0.0f;
-            }
-            b.viewCount++;
+        // View
+        if (root.isMember("View")) {
+            b.viewList = make_unique<ViewList>();
+            b.viewList->fromJson(root["View"]);
         }
 
         // RadarScreen
         if (root.isMember("RadarScreen") && root["RadarScreen"]["vector"].size() == 3) {
-            b.radarScreen.x = root["RadarScreen"]["vector"][0].asFloat();
-            b.radarScreen.y = root["RadarScreen"]["vector"][1].asFloat();
-            b.radarScreen.z = root["RadarScreen"]["vector"][2].asFloat();
-            b.radarScreen.size = root["RadarScreen"]["size"].asFloat();
-            b.radarScreen.tilt = root["RadarScreen"]["tilt"].asFloat();
-            b.hasRadarScreen = true;
-        } else {
-            b.hasRadarScreen = false;
+            b.radarScreen = make_unique<RadarScreen>();
+            b.radarScreen->x = root["RadarScreen"]["vector"][0].asFloat();
+            b.radarScreen->y = root["RadarScreen"]["vector"][1].asFloat();
+            b.radarScreen->z = root["RadarScreen"]["vector"][2].asFloat();
+            b.radarScreen->size = root["RadarScreen"]["size"].asFloat();
+            b.radarScreen->tilt = root["RadarScreen"]["tilt"].asFloat();
         }
 
+        //Throttle
         for (int i = 0; i < 3; ++i) {
             b.portThrottle[i] = root["PortThrottle"][i].asFloat();
             b.stbdThrottle[i] = root["StbdThrottle"][i].asFloat();
@@ -66,141 +104,125 @@ vector<Boat> BoatManager::loadBoats(const string& folderPath) {
 
         // Dynamics
         if (root.isMember("Dynamics")) {
+            b.dynamics = make_unique<Dynamics>();
             for (int i = 0; i < 2; ++i) {
-                b.dynamics.speed[i] = root["Dynamics"]["speed"][i].asFloat();
-                b.dynamics.turnDrag[i] = root["Dynamics"]["turnDrag"][i].asFloat();
-                b.dynamics.lateralDrag[i] = root["Dynamics"]["lateralDrag"][i].asFloat();
+                b.dynamics->speed[i] = root["Dynamics"]["speed"][i].asFloat();
+                b.dynamics->turnDrag[i] = root["Dynamics"]["turnDrag"][i].asFloat();
+                b.dynamics->lateralDrag[i] = root["Dynamics"]["lateralDrag"][i].asFloat();
             }
-            b.hasDynamics = true;
-        } else {
-            b.hasDynamics = false;
         }
 
+        // Propu FOrce
         b.maxPropulsionForce = root.get("Max_propulsion_force", 0.0).asFloat();
+
+        // AsternEfficiency
         b.asternEfficiency = root.get("AsternEfficiency", 1.0).asFloat();
+
+        // Block Coeff
         b.blockCoefficient = root.get("BlockCoefficient", 0.0).asFloat();
+
+        // Mass et Inertia
         b.mass = root.get("Mass", 0.0).asFloat();
         b.inertia = root.get("Inertia", 0.0).asFloat();
 
         // Prop
         if (root.isMember("Prop")) {
-            b.prop.space = root["Prop"]["space"].asFloat();
-            b.prop.walkAhead = root["Prop"]["walkAhead"].asFloat();
-            b.prop.walkAstern = root["Prop"]["walkAstern"].asFloat();
-            b.prop.walkDriftEffect = root["Prop"]["walkDriftEffect"].asFloat();
-            b.hasProp = true;
-        } else {
-            b.hasProp = false;
+            b.prop = make_unique<Prop>();
+            b.prop->space = root["Prop"]["space"].asFloat();
+            b.prop->walkAhead = root["Prop"]["walkAhead"].asFloat();
+            b.prop->walkAstern = root["Prop"]["walkAstern"].asFloat();
+            b.prop->walkDriftEffect = root["Prop"]["walkDriftEffect"].asFloat();
         }
 
-        // AzimuthDrive
+        // Azimuth Drive ou jsp ce que ça représente
         if (root.isMember("AzimuthDrive")) {
             const auto& ad = root["AzimuthDrive"];
-            b.azimuthDrive.azimuthDrive = ad["azimuthDrive"].asBool();
-            b.azimuthDrive.astern = ad["astern"].asBool();
-            b.azimuthDrive.aziToLengthRatio = ad["aziToLengthRatio"].asFloat();
-            b.azimuthDrive.engineIdleRPM = ad["engineIdleRPM"].asInt();
-            b.azimuthDrive.clutchEngageRPM = ad["clutchEngageRPM"].asInt();
-            b.azimuthDrive.clutchDisengageRPM = ad["clutchDisengageRPM"].asInt();
-            b.azimuthDrive.engineMaxChangePerSecond = ad["engineMaxChangePerSecond"].asFloat();
-            b.azimuthDrive.maxDegPerSecond = ad["maxDegPerSecond"].asFloat();
-            b.azimuthDrive.thrustLeverMaxChangePerSecond = ad["thrustLeverMaxChangePerSecond"].asFloat();
-            b.azimuthDrive.sameDirectionAsSchottel = ad["sameDirectionAsSchottel"].asFloat();
-            b.hasAzimuthDrive = true;
-        } else {
-            b.hasAzimuthDrive = false;
+            b.azimuthDrive = make_unique<AzimuthDrive>();
+            b.azimuthDrive->azimuthDrive = ad["azimuthDrive"].asBool();
+            b.azimuthDrive->astern = ad["astern"].asBool();
+            b.azimuthDrive->aziToLengthRatio = ad["aziToLengthRatio"].asFloat();
+            b.azimuthDrive->engineIdleRPM = ad["engineIdleRPM"].asInt();
+            b.azimuthDrive->clutchEngageRPM = ad["clutchEngageRPM"].asInt();
+            b.azimuthDrive->clutchDisengageRPM = ad["clutchDisengageRPM"].asInt();
+            b.azimuthDrive->engineMaxChangePerSecond = ad["engineMaxChangePerSecond"].asFloat();
+            b.azimuthDrive->maxDegPerSecond = ad["maxDegPerSecond"].asFloat();
+            b.azimuthDrive->thrustLeverMaxChangePerSecond = ad["thrustLeverMaxChangePerSecond"].asFloat();
+            b.azimuthDrive->sameDirectionAsSchottel = ad["sameDirectionAsSchottel"].asFloat();
         }
 
         // Rudder
         if (root.isMember("Rudder")) {
-            b.rudder.A = root["Rudder"]["A"].asFloat();
-            b.rudder.B = root["Rudder"]["B"].asFloat();
-            b.rudder.BAstern = root["Rudder"]["BAstern"].asFloat();
-            b.hasRudder = true;
-        } else {
-            b.hasRudder = false;
+            b.rudder = make_unique<Rudder>();
+            b.rudder->A = root["Rudder"]["A"].asFloat();
+            b.rudder->B = root["Rudder"]["B"].asFloat();
+            b.rudder->BAstern = root["Rudder"]["BAstern"].asFloat();
         }
 
+        // Buffet
         b.buffet = root.get("Buffet", 0.0).asFloat();
+
+        // Swell
         b.swell = root.get("Swell", 0.0).asFloat();
+
+        // Windage
         b.windage = root.get("Windage", 0.0).asFloat();
         b.windageTurnEffect = root.get("WindageTurnEffect", 0.0).asFloat();
+
+        // Deviation Max
         b.deviationMaximum = root.get("DeviationMaximum", 0.0).asFloat();
         b.deviationMaximumHeading = root.get("DeviationMaximumHeading", 0.0).asFloat();
+
+        // Period
         b.rollPeriod = root.get("RollPeriod", 0.0).asFloat();
         b.pitchPeriod = root.get("PitchPeriod", 0.0).asFloat();
+
+        // MaxRevs
         b.maxRevs = root.get("MaxRevs", 0).asInt();
+
+        // MaxSpeed Ahead
         b.maxSpeedAhead = root.get("MaxSpeedAhead", 0.0).asFloat();
+
+        // Drift EFfect
         b.centrifugalDriftEffect = root.get("centrifugalDriftEffect", 0.0).asFloat();
+
+        //RateOfTurnIndicator
         b.hasRateOfTurnIndicator = root.get("HasRateOfTurnIndicator", false).asBool();
 
-        // BowThruster
+        // Bow Thruster
         if (root.isMember("BowThruster")) {
-            b.bowThruster.Force = root["BowThruster"]["Force"].asFloat();
-            b.bowThruster.Distance = root["BowThruster"]["Distance"].asFloat();
-            b.hasBowThruster = true;
-        } else {
-            b.hasBowThruster = false;
+            b.bowThruster = make_unique<Thruster>();
+            b.bowThruster->Force = root["BowThruster"]["Force"].asFloat();
+            b.bowThruster->Distance = root["BowThruster"]["Distance"].asFloat();
         }
 
-        // SternThruster
+        // Stern Thruster
         if (root.isMember("SternThruster")) {
-            b.sternThruster.Force = root["SternThruster"]["Force"].asFloat();
-            b.sternThruster.Distance = root["SternThruster"]["Distance"].asFloat();
-            b.hasSternThruster = true;
-        } else {
-            b.hasSternThruster = false;
+            b.sternThruster = make_unique<Thruster>();
+            b.sternThruster->Force = root["SternThruster"]["Force"].asFloat();
+            b.sternThruster->Distance = root["SternThruster"]["Distance"].asFloat();
         }
 
-        // Wheel
+        // Wheel 
         if (root.isMember("Wheel") && root["Wheel"]["vector"].size() == 3) {
-            b.wheel.x = root["Wheel"]["vector"][0].asFloat();
-            b.wheel.y = root["Wheel"]["vector"][1].asFloat();
-            b.wheel.z = root["Wheel"]["vector"][2].asFloat();
-            b.wheel.scale = root["Wheel"]["scale"].asFloat();
-            b.hasWheel = true;
-        } else {
-            b.hasWheel = false;
+            b.wheel = make_unique<Wheel>();
+            b.wheel->x = root["Wheel"]["vector"][0].asFloat();
+            b.wheel->y = root["Wheel"]["vector"][1].asFloat();
+            b.wheel->z = root["Wheel"]["vector"][2].asFloat();
+            b.wheel->scale = root["Wheel"]["scale"].asFloat();
         }
 
         // Sails
-        b.hasSails = false;
-        b.sails.sailCount = 0;
         if (root.isMember("Sails")) {
-            const auto& sailsList = root["Sails"]["list"];
-            for (const auto& sail : sailsList) {
-                if (b.sails.sailCount >= MAX_SAILS || sail.size() < 3) break;
-                b.sails.list[b.sails.sailCount][0] = sail[0].asFloat();
-                b.sails.list[b.sails.sailCount][1] = sail[1].asFloat();
-                b.sails.list[b.sails.sailCount][2] = sail[2].asFloat();
-                b.sails.sailCount++;
-            }
-            b.sails.type = root["Sails"]["type"].asString();
-            b.sails.size = root["Sails"]["size"].asString();
-            b.hasSails = true;
+            b.sails = make_unique<SailList>();
+            b.sails->fromJson(root["Sails"]);
         }
 
         // Pano
-        b.hasPano = false;
-        b.pano.panoCount = 0;
         if (root.isMember("Pano")) {
-            const auto& files = root["Pano"]["file"];
-            const auto& yaws = root["Pano"]["yaw"];
-            const auto& pitches = root["Pano"]["pitch"];
-            const auto& rolls = root["Pano"]["roll"];
-
-            int count = std::min({ files.size(), yaws.size(), pitches.size(), rolls.size(), MAX_PANOS });
-            for (int i = 0; i < count; ++i) {
-                b.pano.file[i] = files[i].asString();
-                b.pano.yaw[i] = yaws[i].asFloat();
-                b.pano.pitch[i] = pitches[i].asFloat();
-                b.pano.roll[i] = rolls[i].asFloat();
-            }
-            b.pano.panoCount = count;
-            b.hasPano = true;
+            b.pano = make_unique<PanoList>();
+            b.pano->fromJson(root["Pano"]);
         }
 
-        // Ajout final du bateau à la liste
         boats.push_back(b);
     }
 
--- Interface/src/BoatManager.hpp
+++ Interface/src/BoatManager.hpp
@@ -1,10 +1,39 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <memory>
+#include <json/json.h>
 
-constexpr int MAX_VIEWS = 6;
-constexpr int MAX_SAILS = 10;
-constexpr int MAX_PANOS = 6;
+// Classes dynamiques
+
+struct View {
+    float x, y, z;
+    bool elevated;
+};
+
+struct ViewList {
+    std::vector<View> views;
+    void fromJson(const Json::Value& json);
+};
+
+struct Sail {
+    float x, y, z;
+};
+
+struct SailList {
+    std::vector<Sail> sails;
+    std::string type;
+    std::string size;
+    void fromJson(const Json::Value& json);
+};
+
+struct PanoList {
+    std::vector<std::string> file;
+    std::vector<float> yaw, pitch, roll;
+    void fromJson(const Json::Value& json);
+};
+
+// Structures simples
 
 struct RadarScreen {
     float x, y, z;
@@ -39,14 +68,11 @@ struct AzimuthDrive {
 };
 
 struct Rudder {
-    float A;
-    float B;
-    float BAstern;
+    float A, B, BAstern;
 };
 
 struct Thruster {
-    float Force;
-    float Distance;
+    float Force, Distance;
 };
 
 struct Wheel {
@@ -54,22 +80,10 @@ struct Wheel {
     float scale;
 };
 
-struct Sails {
-    float list[MAX_SAILS][3];
-    int sailCount;
-    std::string type;
-    std::string size;
-};
+// Classe principale
 
-struct Pano {
-    std::string file[MAX_PANOS];
-    float yaw[MAX_PANOS];
-    float pitch[MAX_PANOS];
-    float roll[MAX_PANOS];
-    int panoCount;
-};
-
-struct Boat {
+class Boat {
+public:
     std::string displayName;
     std::string fileName;
     float scaleFactor;
@@ -81,61 +95,34 @@ struct Boat {
     float maxDepth;
     bool makeTransparent;
 
-    float views[MAX_VIEWS][4];
-    int viewCount;
-
-    RadarScreen radarScreen;
-    bool hasRadarScreen;
-
+    std::unique_ptr<ViewList> viewList;
+    std::unique_ptr<RadarScreen> radarScreen;
     float portThrottle[3];
     float stbdThrottle[3];
-
-    Dynamics dynamics;
-    bool hasDynamics;
-
+    std::unique_ptr<Dynamics> dynamics;
     float maxPropulsionForce;
     float asternEfficiency;
     float blockCoefficient;
     float mass;
     float inertia;
-
-    Prop prop;
-    bool hasProp;
-
-    AzimuthDrive azimuthDrive;
-    bool hasAzimuthDrive;
-
-    Rudder rudder;
-    bool hasRudder;
-
-    float buffet;
-    float swell;
-    float windage;
-    float windageTurnEffect;
-    float deviationMaximum;
-    float deviationMaximumHeading;
-    float rollPeriod;
-    float pitchPeriod;
+    std::unique_ptr<Prop> prop;
+    std::unique_ptr<AzimuthDrive> azimuthDrive;
+    std::unique_ptr<Rudder> rudder;
+    float buffet, swell, windage, windageTurnEffect;
+    float deviationMaximum, deviationMaximumHeading;
+    float rollPeriod, pitchPeriod;
     int maxRevs;
     float maxSpeedAhead;
     float centrifugalDriftEffect;
     bool hasRateOfTurnIndicator;
-
-    Thruster bowThruster;
-    bool hasBowThruster;
-
-    Thruster sternThruster;
-    bool hasSternThruster;
-
-    Wheel wheel;
-    bool hasWheel;
-
-    Sails sails;
-    bool hasSails;
-
-    Pano pano;
-    bool hasPano;
+    std::unique_ptr<Thruster> bowThruster;
+    std::unique_ptr<Thruster> sternThruster;
+    std::unique_ptr<Wheel> wheel;
+    std::unique_ptr<SailList> sails;
+    std::unique_ptr<PanoList> pano;
 };
+
+// Manager
 
 class BoatManager {
 public:
