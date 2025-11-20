037c17ac0513c77d024895bcc3b02c0edbbc8ea4
JusiBridou <juslamarcade@gmail.com>
2025-11-20 11:01:46 +0100

    Passage en gtk4

--- Interface/src/BoatRow.cpp
+++ Interface/src/BoatRow.cpp
@@ -1,20 +1,22 @@
 #include "BoatRow.hpp"
 #include <iostream> // a été utile pour debug donc je le laisse au cas où
+#include <giomm/file.h>
+#include <gtkmm/picture.h>
 
 BoatRow::BoatRow(const Boat& boat) : 
-box(Gtk::ORIENTATION_HORIZONTAL, 10), 
+box(Gtk::Orientation::HORIZONTAL, 10), 
 nameLabel(boat.displayName), 
 menuButton("⋯") // Unicode pour "..."
 {
-
     // Charger une image par défaut
     try {
-        auto pixbuf = Gdk::Pixbuf::create_from_file("../ressources/default.png", 48, 48); // largeur, hauteur
-        image.set(pixbuf);
-    } catch (const Glib::FileError& e) {
+        auto file = Gio::File::create_for_path("../ressources/default.png");
+        auto texture = Gdk::Texture::create_from_file(file);
+        image.set(texture);
+    } catch (const Glib::Error& e) {
         std::cerr << "Erreur chargement image : " << e.what() << std::endl;
     }
-    
+
     // Définir une taille fixe pour l'image (optionnel)
     image.set_pixel_size(12);
 
@@ -23,16 +25,15 @@ menuButton("⋯") // Unicode pour "..."
     box.set_margin_bottom(5);
     box.set_margin_start(10);
     box.set_margin_end(10);
-    box.set_valign(Gtk::ALIGN_CENTER);
+    box.set_valign(Gtk::Align::CENTER);
 
     // Ajout des widgets dans la ligne
-    box.pack_start(image, Gtk::PACK_SHRINK);
-    box.pack_start(nameLabel, Gtk::PACK_EXPAND_WIDGET);
-    box.pack_start(menuButton, Gtk::PACK_SHRINK);
+    box.append(image);        // remplace pack_start
+    box.append(nameLabel);
+    box.append(menuButton);
 
     // Ajouter le conteneur à la ligne
-    add(box);
+    set_child(box);           // remplace add()
 
-    // Afficher tous les enfants
-    show_all_children();
+    // show_all_children() supprimé en GTK 4
 }
