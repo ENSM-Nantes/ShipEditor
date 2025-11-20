6bcc764bc91472643db0334d09118500e46c2302
Jusi <juslamarcade@gmail.com>
2025-11-20 12:10:46 +0100

    Add main function for GTK application

---
+++ Interface/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "main_window.h"
+
+int main (int argc, char *argv[]) {
+  auto app = Gtk::Application::create(argc, argv, "ch.agl-developpement.cpp-tutorial.raspberry-cpp-gtk-opencv");
+  MainWindow mainWindow(300, 300);
+  return app->run(mainWindow);
+}
