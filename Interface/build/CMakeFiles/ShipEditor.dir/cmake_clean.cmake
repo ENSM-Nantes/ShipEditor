bff4a78fffbb54994e1e5dfb598a4cd6dc8cc783
JusiBridou <juslamarcade@gmail.com>
2025-11-26 14:52:55 +0100

    CMAKELIST

---
+++ Interface/build/CMakeFiles/ShipEditor.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShipEditor.dir/BoatManager.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/BoatManager.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/BoatRow.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/BoatRow.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/main.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/main.cpp.obj.d"
+  "ShipEditor.exe"
+  "ShipEditor.exe.manifest"
+  "ShipEditor.pdb"
+  "libShipEditor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShipEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Interface/build/CMakeFiles/ShipEditor.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ShipEditor.
+# This may be replaced when dependencies are built.
