d69e40410213f851aced793ece4acef49bb32688
alexElarte <amestre1215@gmail.com>
2025-12-01 15:32:27 +0100

    Commit from merging

---
+++ Interface/build/CMakeFiles/ShipEditor.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShipEditor.dir/BoatManager.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/BoatManager.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/BoatRow.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/BoatRow.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/input_area.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/input_area.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/main.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/main.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/main_window.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/main_window.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/section/compass_section.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/section/compass_section.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/section/general_section.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/section/general_section.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/section/propertie_section.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/section/propertie_section.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/section/radar_screen_section.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/section/radar_screen_section.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/section/rudder_section.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/section/rudder_section.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/section/section.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/section/section.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/section/weather_section.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/section/weather_section.cpp.obj.d"
+  "CMakeFiles/ShipEditor.dir/section/wheel_section.cpp.obj"
+  "CMakeFiles/ShipEditor.dir/section/wheel_section.cpp.obj.d"
+  "ShipEditor.exe"
+  "ShipEditor.exe.manifest"
+  "ShipEditor.pdb"
+  "libShipEditor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShipEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
